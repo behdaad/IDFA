e8a7dc73ecc94001af66f10f6a8a96ffa3236bad
Behdad Keynejad <behdad.keynejad@gmail.com>
2021-02-08 18:26:56 +0330

    Initial Commit

---
+++ IDFA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDFA
+//
+//  Created by Behdad Keynejad on 11/20/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
